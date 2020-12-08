0e2362e162ce34c09ef3368373acd03e86899904
benjaminparisel <benjamin.parisel@bonitasoft.com>
2020-12-08 09:44:39 +0100

    chore(updateVersion): bump version for babel bonita preset

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-module.exports = {
+module.exports = function () {
+  return {
   plugins: [
-    require("babel-plugin-check-es2015-constants"),
-    require("babel-plugin-transform-es2015-arrow-functions"),
-    require("babel-plugin-transform-es2015-block-scoped-functions"),
-    require("babel-plugin-transform-es2015-block-scoping"),
-    [require("babel-plugin-transform-es2015-classes"), { loose: true } ], // avoid ie10 and below issues with `extends` and `super`
-    require("babel-plugin-transform-es2015-computed-properties"),
-    require("babel-plugin-transform-es2015-destructuring"),
-    require("babel-plugin-transform-es2015-for-of"),
-    require("babel-plugin-transform-es2015-literals"),
-    require("babel-plugin-transform-es2015-modules-commonjs"),
-    require("babel-plugin-transform-es2015-object-super"),
-    require("babel-plugin-transform-es2015-parameters"),
-    require("babel-plugin-transform-es2015-shorthand-properties"),
-    require("babel-plugin-transform-es2015-spread"),
-    require("babel-plugin-transform-es2015-sticky-regex"),
-    require("babel-plugin-transform-es2015-template-literals"),
-    require("babel-plugin-transform-es2015-typeof-symbol"),
-    require("babel-plugin-transform-es2015-unicode-regex"),
-    require("babel-plugin-transform-object-assign"),
-    require("babel-plugin-transform-strict-mode")
-  ]
+    require("@babel/plugin-check-constants"),
+    require("@babel/plugin-transform-arrow-functions"),
+    require("@babel/plugin-transform-block-scoped-functions"),
+    require("@babel/plugin-transform-block-scoping"),
+    [require("@babel/plugin-transform-classes"), {loose: true}], // avoid ie10 and below issues with `extends` and `super`
+    require("@babel/plugin-transform-computed-properties"),
+    require("@babel/plugin-transform-destructuring"),
+    require("@babel/plugin-transform-for-of"),
+    require("@babel/plugin-transform-literals"),
+    require("@babel/plugin-transform-modules-commonjs"),
+    require("@babel/plugin-transform-object-super"),
+    require("@babel/plugin-transform-parameters"),
+    require("@babel/plugin-transform-shorthand-properties"),
+    require("@babel/plugin-transform-spread"),
+    require("@babel/plugin-transform-sticky-regex"),
+    require("@babel/plugin-transform-template-literals"),
+    require("@babel/plugin-transform-typeof-symbol"),
+    require("@babel/plugin-transform-unicode-regex"),
+    require("@babel/plugin-transform-object-assign"),
+    require("@babel/plugin-transform-strict-mode")
+  ]};
 };
